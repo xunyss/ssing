60b05511f2a3d1d2c29ccbe869d3523f1db74b60
xunyss <xunyss@gmail.com>
2016-11-25 03:38:42 +0900

    shell

--- ssing-cli/shell/ssing.cmd
+++ ssing-cli/shell/ssing.cmd
@@ -5,6 +5,6 @@ set CLASSPATH=            D:/xdev/git/ssing/ssing-cli/target/dependency/*
 set CLASSPATH=%CLASSPATH%;D:/xdev/git/ssing/ssing-cli/target/classes
 REM CLASSPATH=%CLASSPATH%;D:/xdev/git/ssing/ssing-cli/target/ssing-cli-0.0.1-SNAPSHOT.jar
 
-set STARTCLASS=com.xunyss.ssing.cli.CliLauncher
+set STARTCLASS=io.xunyss.ssing.cli.CliLauncher
 
 java -classpath %CLASSPATH% %STARTCLASS% %*
