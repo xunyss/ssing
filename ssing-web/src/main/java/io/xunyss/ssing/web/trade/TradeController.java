fb8aa3866d42ca68aedb9c9032b4fa0862b8151f
xunyss <xunyss@gmail.com>
2016-12-08 01:51:29 +0900

    add remote-shell

--- ssing-web/src/main/java/io/xunyss/ssing/web/trade/TradeController.java
+++ ssing-web/src/main/java/io/xunyss/ssing/web/trade/TradeController.java
@@ -1,11 +1,7 @@
 package io.xunyss.ssing.web.trade;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import io.xunyss.ssing.api.Session;
 
 /**
  * 
